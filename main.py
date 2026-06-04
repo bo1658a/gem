77dd5873fc4e35cdb2cba321f1f49969ff92d0ec
Bob Brhayan Okoth <78672484+bo1658a@users.noreply.github.com>
2026-06-04 10:31:34 +0300

    Implement frame rate control in main loop
    Added clock for frame rate control and delta time calculation.

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 
-
 def main() -> None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -17,9 +18,10 @@ def main() -> None:
 
         screen.fill("black")
         pygame.display.flip()
-        
 
-    
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
